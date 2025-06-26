71b8c16ce9a605f9a2fe0d12f64e6a4bf7d1e76c
Ben Stanley <benmastanley@gmail.com>
2025-06-26 13:09:02 +0100

    add initial code and gitignore

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,4 +1,24 @@
 function MovieCard({ movie }) {
-  return;
-  <div className="movie-card"></div>;
+  function onFavouriteClick() {
+    alert("Clicked");
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} />
+        <div className="movie-overlay">
+          <button className="favourite-button" onClick={onFavouriteClick}>
+            ♥
+          </button>{" "}
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
+    </div>
+  );
 }
+
+export default MovieCard;
